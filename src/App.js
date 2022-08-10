bdcd62544ae50c212843cfa484d7cb56a05df129
tanimpersonal <tashintanim2019@gmail.com>
2022-08-11 00:27:10 +0600

    header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Route, Routes } from "react-router-dom";
+import AdminPanel from "./Components/AdminPanel/AdminPanel";
+import Header from "./Components/Header/Header";
+import HomePage from "./Components/HomePage/HomePage";
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<HomePage></HomePage>}></Route>
+        <Route path="/admin-panel" element={<AdminPanel></AdminPanel>}></Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
